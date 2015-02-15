b1f8a09c3376f83e1670e407921889be8c0a6dfe
Selmaan <smaaan@gmail.com>
2015-02-15 13:04:14 -0500

    conflict resolve

--- lassoScript.m
+++ lassoScript.m
@@ -3,7 +3,7 @@
 % sig = de(neur,:);
 
 %% Create Predictor Matrix
-Y_scale = 3;
+Y_scale = 1;
 shifts = [0 1 2 3 5 8 13 21 34 55 89 144 233];
 %shifts = [0:1:10 12 15 19 24 30 40 55 75 100 130 170];
 shifts = [-fliplr(shifts(2:end)),shifts];
@@ -46,7 +46,7 @@ fitCoef = cvglmnetCoef(CVerr);
 p = reshape(fitCoef(2:end),3,[]);
 x=-750:750;
 b = zeros(1,1.5e3+1);
-b(751) = 1;
+b(751) = x1;
 
 bases = [];
 for shift = 1:length(shifts)
